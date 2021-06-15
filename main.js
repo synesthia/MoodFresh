5498c86f31dd64fc0343eb30ed8c515cdb85b63e
ab <nomail>
2021-06-15 18:31:08 +0300

    the new design seems to work

--- main.js
+++ main.js
@@ -3,25 +3,25 @@ const idx_fill = 1;
 const idx_amount = 2;
 const idx_mony = 3;
 const idx_func_add = 4;
-
-var Portocale = ["Portocale.png", 'orange', 100, 3, function() {add_fruit_param('Portocale');} ];
+/*
+//var Portocale = ["Portocale.png", 'orange', 100, 3, function() {add_fruit_param('Portocale');} ];
 var Mar = ["Mar.png", '#8db600', 100, 2, function() {add_fruit_param('Mar');} ];
-var Banane = ["Banane.png", '#ffe135', 100, 3, function() {add_fruit_param('Banane');} ];
+//var Banane = ["Banane.png", '#ffe135', 100, 3, function() {add_fruit_param('Banane');} ];
 var Lamaie = ["Lamaie.png", '#fff700', 50, 2, function() {add_fruit_param('Lamaie');} ];
 var Lime = ["Lime.png", '#bfff00', 20, 2, function() {add_fruit_param('Lime');} ];
 var Kiwi = ["Kiwi.png", '#8ee53f', 100, 4, function() {add_fruit_param('Kiwi');} ];
 var Ananas = ["Ananas.png", '#fee88f', 100, 4, function() {add_fruit_param('Ananas');} ];
-var Cocos = ["Cocos.png", 'white', 20, 3, function() {add_fruit_param('Cocos');} ];
+//var Cocos = ["Cocos.png", 'white', 20, 3, function() {add_fruit_param('Cocos');} ];
 var Grapefruit = ["Grapefruit.png", '#ff6677', 100, 3, function() {add_fruit_param('Grapefruit');} ];
 
 //make_button("Portocale");
 make_button("Mar");
-make_button("Banane");
+//make_button("Banane");
 make_button("Lamaie");
 make_button("Lime");
 make_button("Kiwi");
 make_button("Ananas");
-make_button("Cocos");
+//make_button("Cocos");
 make_button("Grapefruit");
 
 var Castravete = ["Castravete.png", '#8ABB55', 100, 3, function() {add_fruit_param('Castravete');} ];
@@ -55,7 +55,7 @@ make_button("Scortisoara");
 make_button("Ghimbir");
 make_button("Canepa");
 make_button("Turmeric");
-
+*/
 var Key = 'name';
 var Fields = [Key, 'color', 'grams', 'price'];
 var Fruits_data = [
@@ -63,6 +63,10 @@ var Fruits_data = [
   ["Banane", '#ffe135', 100, 3],
   ["Cocos", 'white', 20, 3]
 ];
+
+make_btn("Portocale");
+make_btn("Banane");
+make_btn("Cocos");
 
 var Fruits = new Map();
 
@@ -87,25 +91,28 @@ Fruits_data.forEach(make_Fruits);
 var List = new Map();
 
 List.add = function(fruit_name) {
-  if (List.has(fruit_name)) {
-    this.get(fruit_name).gr += Fruits.get(fruit_name).gr;
+  if (this.has(fruit_name)) {
+    var gr = this.get(fruit_name);
+    gr += Fruits.get(fruit_name).grams;
+    this.set(fruit_name, gr);
   }
   else {
-    this.set(fruit_name, Object());
-    this.get(fruit_name).gr = Fruits.get(fruit_name).gr;
+    this.set(fruit_name, Fruits.get(fruit_name).grams);
   }
 }
 
 List.remove = function(fruit_name) {
-  if (List.has(fruit_name)) {
-    this.get(fruit_name).gr -= Fruits.get(fruit_name).gr;
-    if (this.get(fruit_name).gr <= 0) {
+  if (this.has(fruit_name)) {
+    var gr = this.get(fruit_name);
+    gr -= Fruits.get(fruit_name).grams;
+    if (gr <= 0) {
       this.delete(fruit_name);
+    }
+    else {
+      this.set(fruit_name, gr);
     }
   }
 }
-
-make_btn("Portocale");
 
 function make_btn(fruit) {
   var button;
@@ -114,8 +121,25 @@ function make_btn(fruit) {
   button.src = fruit + '.png';
 
   button.onclick = function() {
-    add_fruit_param(fruit);
+    add_fruit_new(fruit);
   };
+}
+
+var History = [];
+
+function add_fruit_new(fruit_name) {
+  if ( Canvas.level < Mare - Fruits.get(fruit_name).grams ) {
+    Canvas.fill(Fruits.get(fruit_name).color, Fruits.get(fruit_name).grams, Fruits.get(fruit_name).price);
+
+    List.add(fruit_name);
+    History.push(fruit_name);
+
+    show_total_new();
+  }
+
+  if ( Canvas.level >= Mic ) {
+    order_green();
+  }
 }
 
 var total = 0;
@@ -143,32 +167,25 @@ Canvas.back = function(gr) {
 
 var ingredients = [];
 
-var List = Object();
-
-List.add = function(name, gr, price) {
-  if (List.hasOwnProperty(name)) {
-    this[name] += 3;
-  }
-  else {
-    this[name] = 3;
-  }
-}
-
-List.remove = function(name) {
-  if (List.hasOwnProperty(name)) {
-    this[name] -= 3;
-    if (this[name] <= 0) {
-      delete this[name];
-    }
-  }
-}
-
 var Gui = Object();
 Gui.setList = function(total_sum, list_item, mililiters, total_mililiters) {
   document.getElementById('total').innerHTML = total_sum;
   document.getElementById('list_item').innerHTML = list_item;
   document.getElementById('ml').innerHTML = mililiters;
   document.getElementById('total_ml').innerHTML = total_mililiters;
+}
+
+function show_total_new() {
+  var li = '';
+  var ml = '';
+  for (var [fr, gr] of List ) {
+    li += fr + "<br />";
+    ml += gr + ' ml.' + "<br />";
+  }
+  var ttml = "-------<br />";
+  ttml += Canvas.level + ' ml.';
+
+  Gui.setList(total + ' RON', li, ml, ttml);
 }
 
 function show_total() {
@@ -209,6 +226,30 @@ function add_fruit(name, color, gr, price) {
 }
 
 function remove_fruit() {
+  if (History.length <= 0) {
+    return;
+  }
+
+  if ( Canvas.level > 0 ) {
+    fruit_name = History.pop();
+
+    var gr = Fruits.get(fruit_name).grams;
+    Canvas.back(gr);
+
+    List.remove(fruit_name);
+
+    total -= Fruits.get(fruit_name).price;
+    show_total_new();
+  }
+  if ( Canvas.level < Mic ) {
+    order_gray();
+  }
+  else {
+    order_green();
+  }
+}
+
+function remove_fruit_old() {
   if ( Canvas.level > 0 ) {
     price = ingredients.pop();
     gr = ingredients.pop();
